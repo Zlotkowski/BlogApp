838a508ae20ad1cb1593b32e3562b157da3b230d
Zlotkowski <zlotkowskimariusz@gmail.com>
2021-07-06 09:49:14 +0200

    add search bar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,15 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { ThemeContext } from "../ThemeContext";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 
 export default function Navbar() {
+  const [query, setQuery] = useState("initialState");
+  const history = useHistory();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    history.push(`/search/${query}`);
+  };
   const { theme, toggleTheme } = useContext(ThemeContext);
   return (
     <div className="header">
@@ -10,6 +17,17 @@ export default function Navbar() {
         <Link to="/">
           <strong>Awesome Blog</strong>
         </Link>
+      </div>
+      <div class="header-item">
+        <form onSubmit={handleSubmit}>
+          <input
+            name="query"
+            type="text"
+            placeholder="search posts"
+            onChange={(e) => setQuery(e.target.value)}
+          ></input>
+          <button>Go</button>
+        </form>
       </div>
       <div className="header-item">
         <a href="/login">Login</a>
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import React, { useEffect, useReducer } from "react";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 
 const reducer = (state, action) => {
   switch (action.type) {
@@ -19,6 +19,13 @@ const reducer = (state, action) => {
         users: action.payload,
         errorUsers: "",
       };
+    case "USER_SUCCESS":
+      return {
+        ...state,
+        loadingUsers: false,
+        user: action.payload,
+        errorUsers: "",
+      };
     case "USERS_FAIL":
       return { ...state, errorUsers: action.payload, loadingUsers: false };
     default:
@@ -27,6 +34,7 @@ const reducer = (state, action) => {
 };
 
 export default function HomePage() {
+  const { query, userId } = useParams();
   const [state, dispatch] = useReducer(reducer, {
     loading: false,
     error: "",
@@ -34,17 +42,28 @@ export default function HomePage() {
     loadingUsers: false,
     errorUsers: "",
     users: [],
+    user: {},
   });
 
-  const { loading, error, posts, loadingUsers, errorUsers, users } = state;
+  const { loading, error, posts, loadingUsers, errorUsers, users, user } =
+    state;
 
   const loadPosts = async () => {
     dispatch({ type: "POSTS_REQUEST" });
     try {
       const { data } = await axios.get(
-        "https://jsonplaceholder.typicode.com/posts"
+        userId
+          ? "https://jsonplaceholder.typicode.com/posts?userId=" + userId
+          : "https://jsonplaceholder.typicode.com/posts"
       );
-      dispatch({ type: "POSTS_SUCCESS", payload: data });
+      const filteredPosts = query
+        ? data.filter(
+            (searchData) =>
+              searchData.title.indexOf(query) >= 0 ||
+              searchData.body.indexOf(query) >= 0
+          )
+        : data;
+      dispatch({ type: "POSTS_SUCCESS", payload: filteredPosts });
     } catch (error) {
       dispatch({ type: "POSTS_FAIL", payload: error.message });
     }
@@ -54,9 +73,14 @@ export default function HomePage() {
     dispatch({ type: "USERS_REQUEST" });
     try {
       const { data } = await axios.get(
-        "https://jsonplaceholder.typicode.com/users"
+        userId
+          ? "https://jsonplaceholder.typicode.com/users/" + userId
+          : "https://jsonplaceholder.typicode.com/users"
       );
-      dispatch({ type: "USERS_SUCCESS", payload: data });
+      dispatch({
+        type: userId ? "USER_SUCCESS" : "USERS_SUCCESS",
+        payload: data,
+      });
     } catch (error) {
       dispatch({ type: "USERS_FAIL", payload: error.message });
     }
@@ -65,12 +89,18 @@ export default function HomePage() {
   useEffect(() => {
     loadPosts();
     loadUsers();
-  }, []);
+  }, [query, userId]);
 
   return (
     <div className="blog">
       <div className="content">
-        <h1>Posts</h1>
+        <h1>
+          {query
+            ? `Results for "${query}"`
+            : userId
+            ? `${user.name}'s Posts`
+            : "Posts"}
+        </h1>
         {loading ? (
           <div>Loading...</div>
         ) : error ? (
@@ -89,19 +119,32 @@ export default function HomePage() {
         )}
       </div>
       <div className="sidebar">
-        <h2>Authors</h2>
         {loadingUsers ? (
           <div>Loading...</div>
         ) : errorUsers ? (
           <div>Error: {errorUsers}</div>
         ) : users.length === 0 ? (
           <div>No user found</div>
+        ) : userId ? (
+          <div>
+            <h2>{user.name}'s Profile</h2>
+            <ul>
+              <li>Email: {user.email}</li>
+              <li>Phone: {user.phone}</li>
+              <li>Website: {user.website}</li>
+            </ul>
+          </div>
         ) : (
-          <ul>
-            {users.map((user) => (
-              <li key={user.id}> {user.name}</li>
-            ))}
-          </ul>
+          <div>
+            <h2>Authors</h2>
+            <ul>
+              {users.map((user) => (
+                <li key={user.id}>
+                  <Link to={`/user/${user.id}`}>{user.name}</Link>
+                </li>
+              ))}
+            </ul>
+          </div>
         )}
       </div>
     </div>
